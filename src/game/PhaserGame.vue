30954f7546bb66353acf1d3ff8bba2ba8b946683
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-12-16 02:02:41 -0500

    feat: words now come one at a time

--- src/game/PhaserGame.vue
+++ src/game/PhaserGame.vue
@@ -10,44 +10,45 @@ const game = ref();
 const WORDS = ref<string[]>(textFile.split('\r\n'))
 const words_list = ref<string[]>([]);
 const text = ref('')
+const last_removed = ref(-1);
 
 const emit = defineEmits(['current-active-scene', 'words-list', 'word-complete']);
 
 const appendRandomWord = () => {
     const randomInt = Math.floor(Math.random() * WORDS.value.length)
-    words_list.value.push(WORDS.value[randomInt] || ''); 
-}
-
-const generateNewWords = () => {
-    words_list.value = []
-    for(let i = 0; i < 10; i++){
-        appendRandomWord();
+    if(last_removed.value === -1){ //if there is no last removed
+        words_list.value.push(WORDS.value[randomInt] || ''); 
+        console.log(words_list.value)
     }
-    EventBus.emit('words-list', words_list.value);
+    else { //replace most recent one
+        words_list.value.splice(last_removed.value, 0, WORDS.value[randomInt] || ''); 
+    }
 }
 
 
 onMounted(() => {
     game.value = StartGame('game-container');
-    generateNewWords();
-
     EventBus.on('current-scene-ready', (scene_instance: Phaser.Scene) => {
         EventBus.emit('current-active-scene', scene_instance);
-        EventBus.emit('words-list', words_list.value);
         scene.value = scene_instance;
-    
+        EventBus.emit('words-list', words_list.value);
     });
+
+    EventBus.on('add-new-word', ()=> {
+        appendRandomWord();
+        EventBus.emit('words-list', words_list.value);
+    })
 
 });
 
-watch(text, async(newText, oldText)=> {
+watch(text, async(newText, oldText)=> { //when word is typed out
     const index: number = words_list.value.indexOf(newText, 0)
     if(index !== -1){
         text.value = "";
         words_list.value.splice(index, 1);
+        last_removed.value = index;
         appendRandomWord();
         EventBus.emit('word-complete', index);
-        EventBus.emit('words-list', words_list.value);
     }
 })
 
@@ -71,7 +72,6 @@ defineExpose({ scene, game });
     <div className="flex justify-center items-center">
         <div className="flex flex-col">
         <div>
-        <button @click="generateNewWords"> Click Me </button>
         <input v-model="text"> </input>
         </div>
         <p> {{text}} </p>
--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -9,8 +9,20 @@ export class Game extends Scene
     newWordTimeGoal: number = 0;
     runningTimer: number = 0; 
     addNewWord: boolean = false;
+    words: Set<string> = new Set<string>();
 
-    //goal: get words to appear periodically
+    //make text fall down and spawn from random locations
+    //delete game objects when they go off screen
+
+    // private textFall = (x_index: [number, number]) => {
+    //     if(this.texts[x_index[1]].y < 720){
+    //         this.texts[x_index[1]].setPosition(x_index[0], this.texts[x_index[1]].y+=1)
+    //     }
+    //     else{
+    //         this.texts[x_index[1]].destroy();
+    //     }
+
+    // }
 
     constructor ()
     {
@@ -21,25 +33,30 @@ export class Game extends Scene
     {
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
-
-        for(let i = 0; i < 10; i++){
-            this.texts[i] = this.add.text(-100, -100, 'loading...', {
-                fontFamily: 'Arial Black', fontSize: 18, color: '#ffffff',
-                stroke: '#000000', strokeThickness: 4,
-                align: 'center'
-            }).setOrigin(0.5).setDepth(100);
-            
-        }
-
-        EventBus.on('words-list', (data: string[]) => {
-            for(let i = 0; i < 10; i++){
-                this.texts[i].setText(data[i]);
-            }
-        });
+        
 
         EventBus.emit('current-scene-ready', this);
         
         this.newWordTimeGoal = Math.floor(Math.random() * 250 + 10)
+
+        EventBus.on('words-list', (data: string[]) => {
+            for(let i = 0; i < data.length; i++){
+                console.log(this.words)
+                if(!this.words.has(data[i])){
+                    this.texts[i] = this.add.text(-100, -100, 'loading...', {
+                        fontFamily: 'Arial Black', fontSize: 18, color: '#ffffff',
+                        stroke: '#000000', strokeThickness: 4,
+                        align: 'center'
+                    }).setOrigin(0.5).setDepth(100);
+                    
+                    this.texts[i].setText(data[i]);
+                    const x_val = Math.random() * 1080;
+                    this.texts[i].setPosition(x_val, -100);
+
+                    this.words.add(data[i])
+                }
+            }
+            });
         
     }
 
@@ -47,19 +64,29 @@ export class Game extends Scene
 
 
         EventBus.on('word-complete', (data: number) => {
-            console.log(data)
             this.texts[data].setPosition(-100,-100);
         });
+
+
 
         if(this.newWordTimeGoal !== this.runningTimer){
             this.runningTimer++;
         }
-        else{
-            const randomIndex: number = Math.floor(Math.random() * 10)
-            this.texts[randomIndex].setPosition(500, 500);
+        else {
+            console.log("yes")
+            EventBus.emit('add-new-word', true);
+            const newIndex: number = this.texts.length+1 
+            const x_val = Math.random() * 1080;
+
             this.newWordTimeGoal = Math.floor(Math.random() * 250 + 50)
             this.runningTimer = 0; 
         }
+
+        for(const text of this.texts){
+            console.log(text.y+1)
+            text.setPosition(text.x, text.y+1)
+        }
+        
         
     }
 }
